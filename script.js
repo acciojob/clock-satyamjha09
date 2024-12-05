ebd2e2a794a1434f44e85092d087eba554ab3f74
satyam jha <109567398+satyamjha09@users.noreply.github.com>
2024-12-05 18:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 
+// const displayTime = document.getElementById("timer");
+
+// let currentTime = new Date();
+
+// setInterval(
+
+// 	function() {
+// 		currentTime = new Date()
+// 		displayTime.innerHTML = currentTime.toLocaleString();
+// 	},1000
+	
+// )
+
 const displayTime = document.getElementById("timer");
 
-let currentTime = new Date();
+let currentTime = new Date()
 
-setInterval(
+setInterval(() => {
+	currentTime = new Date()
+	displayTime.innerHTML = currentTime.toLocaleString();
+},1000)
 
-	function() {
-
-		currentTime = new Date()
-		displayTime.innerHTML = currentTime.toLocaleString();
-	},1000
-	
-)
